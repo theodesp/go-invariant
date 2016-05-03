97fd21bacff18d8e55aaea06fad0554b53531e6f
Theofanis Despoudis <tdespoudis@quotix.com>
2016-05-03 15:48:34 +0000

    Proper fixes

---
+++ invariant.go
@@ -0,0 +1,46 @@
+package invariant
+
+import (
+        "fmt"        
+		"github.com/namsral/flag"
+)
+
+const (
+	development = "development"
+	production = "production"
+)
+
+type InvariantError struct {
+	What string
+	Name string
+}
+
+func (e InvariantError) Error() string {
+	return fmt.Sprintf("%v", e.What)
+}
+
+func invariant(condition bool, format string, args ...interface{}) error {
+	var env string
+	
+	flag.StringVar(&env, "environment", development, "current environment (development or production)")
+    flag.Parse()
+	
+	if env != production {
+	    if format == "" {
+	      return InvariantError{"invariant requires an error message argument", "Invariant Violation"};
+	    }
+ 	}
+	
+	if !condition {
+		var error InvariantError;
+		
+		if format == "" {
+	      	error = InvariantError{"invariant exception in production environment. Please use development flag to see the full error message", "Invariant Violation"};
+	    } else {
+			error = InvariantError{fmt.Sprintf(format, args...), "Invariant Violation"}
+	    }
+		
+		return error
+	}
+	return nil
+}
---
+++ invariant_test.go
@@ -0,0 +1,18 @@
+package invariant
+
+import (
+    "testing"
+)
+
+func TestFlags(t *testing.T) {
+	t.Skip("skipping test in short mode.")
+}
+
+func TestCondition(t *testing.T) {
+	t.Skip("skipping test in short mode.")
+}
+
+func TestResponses(t *testing.T) {
+	t.Skip("skipping test in short mode.")
+}
+
